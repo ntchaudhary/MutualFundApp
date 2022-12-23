d18770ba25c477ed80ee552ed2eca5df3c6400dd
ntchaudhary <37553212+ntchaudhary@users.noreply.github.com>
2022-12-24 00:24:46 +0530

    oky

--- mutualFundApp/urls.py
+++ mutualFundApp/urls.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from .src.fund_details.index import fundDetails
 from .src.available_fund_list.index import availableFundList
-
+from .src.fund_transactions.index import fundTransactions
 
 mutualFundApp = APIRouter()
 
@@ -9,3 +9,4 @@ _BASE_ENDPOINT = "/mutual-fund"
 
 mutualFundApp.include_router(fundDetails, prefix=_BASE_ENDPOINT)
 mutualFundApp.include_router(availableFundList, prefix=_BASE_ENDPOINT)
+mutualFundApp.include_router(fundTransactions, prefix=_BASE_ENDPOINT)
