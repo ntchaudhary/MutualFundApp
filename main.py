fd2539bdf0aa82f3cda6dd6b37fb4bb5fdaaaed3
ntchaudhary <37553212+ntchaudhary@users.noreply.github.com>
2023-06-24 23:28:14 +0530

    endpoint change

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
-from mutualFundApp.urls import mutualFundApp
+# from mutualFundApp.urls import mutualFundApp
 from website.urls import website
-from depositsApp.urls import depositeApp
+# from depositsApp.urls import depositeApp
 from mangum import Mangum
 
 app = FastAPI()
 
-app.include_router(mutualFundApp)
+# app.include_router(mutualFundApp)
 app.include_router(website)
-app.include_router(depositeApp)
+# app.include_router(depositeApp)
 
 handler = Mangum(app)
