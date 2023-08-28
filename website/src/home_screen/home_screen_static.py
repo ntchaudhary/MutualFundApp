8479cb6cf2f9ee507862a55b6e6116ee658a275d
ntchaudhary <37553212+ntchaudhary@users.noreply.github.com>
2023-08-28 23:26:32 +0530

    fixed nav-bar and removed PF link

---
+++ website/src/home_screen/home_screen_static.py
@@ -0,0 +1,91 @@
+_inactive_data_mapping = {
+    'Provident Fund': {
+        "type": 'Provident Fund',
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": ""
+    },
+}
+
+active_data_mapping = {
+    'Mutual Funds': {
+        "type": 'Mutual Funds',
+        "add_new_url": "/website/add-fund",
+        "details_url": "/website/fund-list",
+        "button_text": "Add New"
+    },
+    'Deposits': {
+        "type": 'Deposits',
+        "add_new_url": "/website/add-deposit",
+        "details_url": "/website/deposit-list",
+        "button_text": "Add New"
+    },
+    'Gold': {
+        "type": 'Gold',
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": "Add New"
+    },
+    'Silver': {
+        "type": 'Silver',
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": "Add New"
+    },
+    'Bank Balance': {
+        "type": 'Bank Balance',
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": "update"
+    },
+    'ETF': {
+        "type": 'ETF',
+        "amount": 0,
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": "coming soon..."
+    },
+    'NPS': {
+        "type": 'National Pension Scheme',
+        "amount": 0,
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": "coming soon..."
+    },
+    'Stocks': {
+        "type": 'Stocks',
+        "amount": 0,
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": "coming soon..."
+    },
+    'Bonds': {
+        "type": 'Bonds',
+        "amount": 0,
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": "coming soon..."
+    },
+    'IPO': {
+        "type": 'IPO',
+        "amount": 0,
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": "coming soon..."
+    },
+    'NSC': {
+        "type": 'National Saving Certificate',
+        "amount": 0,
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": "coming soon..."
+    },
+    'KVP': {
+        "type": 'Kisan Vikas Patra',
+        "amount": 0,
+        "add_new_url": "#",
+        "details_url": "#",
+        "button_text": "coming soon..."
+    }
+
+}
