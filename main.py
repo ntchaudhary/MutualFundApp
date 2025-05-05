e8a8e236af68e20f3f527412238860784ef549ff
ntchaudhary <ntchaudhary@example.com>
2025-05-05 21:40:01 +0530

    branch changes done

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
+# from dotenv import load_dotenv
+
+from middleware.authenticationMiddleware import AuthMiddleware
+
+# load_dotenv(override=True)
+
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
-# from mutualFundApp.urls import mutualFundApp
 from utilities.auth import TokenExpiredException
 from website.urls import website
-# from depositsApp.urls import depositeApp
 from mangum import Mangum
+
 
 app = FastAPI()
 
 # app.include_router(mutualFundApp)
+app.add_middleware(AuthMiddleware)
 app.include_router(website)
 # app.include_router(depositeApp)
-
-
-
-# Exception handler for TokenExpiredException to trigger redirection
-@app.exception_handler(TokenExpiredException)
-async def token_expired_exception_handler(request: Request, exc: TokenExpiredException):
-    # Redirect user to sign-in page if token is expired or invalid
-    return RedirectResponse(url="/sign-in")
 
 
 handler = Mangum(app)
